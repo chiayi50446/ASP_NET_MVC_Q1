4dbee41b3d65667ebfdbaf4dfb3b7f93bbcb8840
Chiayi Lin <angel05446a@gmail.com>
2019-07-15 13:10:45 +0800

    ASP_NET_MVC_Q1

---
+++ MVC_Q1/MVC_Q1/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using MVC_Q1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Q1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public new ActionResult Detail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public new ActionResult Detail(DetailModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(data);
+            }
+            TempData["detail"] = data;
+            return RedirectToAction("Check");
+        }
+
+        public ActionResult Check()
+        {
+            return View(TempData["detail"]);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Check(DetailModel data)
+        {
+            if (!Directory.Exists(@"D:\temp\"))
+            {
+                Directory.CreateDirectory(@"D:\temp\");
+            }
+            string fileName = "Detail-" + DateTime.Now.ToString("HHmmssMMddyyyy");
+            StreamWriter sw = new StreamWriter(@"D:\Temp\" + fileName + ".txt");
+            sw.WriteLine("Name : " + data.Name);
+            sw.WriteLine("Phone : " + data.Phone);
+            sw.WriteLine("Email : " + data.Email);
+            sw.Close();
+            return RedirectToAction("Save", "Data");
+        }
+    }
+}
---
+++ MVC_Q1/MVC_Q1/Models/DetailModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Q1.Models
+{
+    public class DetailModel
+    {
+        public string Name { get; set; }
+        [StringLength(35)]
+        [RegularExpression(@"^\d+$"
+            , ErrorMessage = "Invalid Phone Number"
+        )]
+        public string Phone { get; set; }
+        [Required()]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    public class DetailModelStore
+    {
+        DetailModel detailModel = new DetailModel();
+
+    }
+}
